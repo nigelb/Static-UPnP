5e3d067e1876f22d650071ff5d9c89f3da6d04df
NigelB <nigel.blair@gmail.com>
2017-01-08 22:05:32 +1000

    Rename StaticUPnP_Permissions to StaticUPnP_Settings and add interface settings

--- examples/PanasonicDVR/StaticUPnP_Settings.py
+++ examples/PanasonicDVR/StaticUPnP_Settings.py
@@ -15,5 +15,13 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-user = "nobody"
-group = "nobody"
+permissions = {
+    'drop_permissions': True,
+    'user': "nobody",
+    'group': "nobody"
+}
+
+interfaces = {
+    "include": [],
+    "exclude": []
+}
